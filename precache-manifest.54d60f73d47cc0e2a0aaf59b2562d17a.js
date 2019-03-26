77d3e79098263c9bd13674283f0b8279638b879b
William Lee <karlweiyang@gmail,com>
2019-03-26 09:39:43 +0000

    Updates

---
+++ precache-manifest.54d60f73d47cc0e2a0aaf59b2562d17a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6670768c31502717d35c",
+    "url": "/TicTacToe/static/js/runtime~main.ae000fa4.js"
+  },
+  {
+    "revision": "582ef1e72b38cc1ac419",
+    "url": "/TicTacToe/static/js/main.1b1f4be0.chunk.js"
+  },
+  {
+    "revision": "fef83e67b75a1a6b41d2",
+    "url": "/TicTacToe/static/js/2.71c3979c.chunk.js"
+  },
+  {
+    "revision": "582ef1e72b38cc1ac419",
+    "url": "/TicTacToe/static/css/main.88d8d979.chunk.css"
+  },
+  {
+    "revision": "53f683d25d990ee714e15999c61a53fd",
+    "url": "/TicTacToe/index.html"
+  }
+];
